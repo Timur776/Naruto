c092b79e743ec4efb241b88efead2913995b155f
Timur776 <timurkim772@gmail.com>
2020-01-24 16:52:34 +0300

    task description

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+                Console.WriteLine((n >> p & 1) == 1);
+            
+        }
+    }
+}

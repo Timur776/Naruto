85c010856f3cd97488d785a2d54afc27ec668103
Timur776 <timurkim772@gmail.com>
2020-02-27 21:18:50 +0300

    выведение чисел в поредке убывания

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,10 +6,46 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int m = int.Parse(Console.ReadLine());
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine(Math.Max(m, n));
-
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            
+                if (a <= b && b <= c) 
+                {
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                }
+                if (a <= c && c <= b)
+                {
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                }
+                if (b <= a && a <= c)
+                {
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                }
+                if (b <= c && c <= a)
+                { 
+                Console.WriteLine(c);
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                }
+                if (c <= a && a <= b)
+                {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+                }
+                if (c <= b && b <= a)
+                {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                }
         }
     }
 }

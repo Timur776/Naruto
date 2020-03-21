3fc295a46b3ecaa17bf0925e6c00b6c7f620b18e
Timur776 <timurkim772@gmail.com>
2020-03-21 16:33:26 +0300

    52 карты

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,26 +6,72 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            char n =char.Parse(Console.ReadLine());
-            switch (n)
+            int n = 15;
+            int m = 5;
+
+            for (int i = 2; i < n; i++)
             {
-                case 'i':
-                    int i = int.Parse(Console.ReadLine());
-                    Console.WriteLine(i + 1);
-                    break;
-
-                case 'd':
-                    double d = double.Parse(Console.ReadLine());
-                    Console.WriteLine(d + 1);
-                    break;
-
-                case 's':
-                    string s = Console.ReadLine();
-                    Console.WriteLine(s + '*');
-                    break;
-
+                for (int j = 1; j < m; j++)
+                {
+                    switch (i)
+                    {
+                        case 2:
+                            Console.Write("два ");
+                            break;
+                        case 3:
+                            Console.Write("три ");
+                            break;
+                        case 4:
+                            Console.Write("четыре ");
+                            break;
+                        case 5:
+                            Console.Write("пять ");
+                            break;
+                        case 6:
+                            Console.Write("шесть ");
+                            break;
+                        case 7:
+                            Console.Write("семь ");
+                            break;
+                        case 8:
+                            Console.Write("восемь ");
+                            break;
+                        case 9:
+                            Console.Write("девять ");
+                            break;
+                        case 10:
+                            Console.Write("десять ");
+                            break;
+                        case 11:
+                            Console.Write("волет ");
+                            break;
+                        case 12:
+                            Console.Write("дама ");
+                            break;
+                        case 13:
+                            Console.Write("колроь ");
+                            break;
+                        case 14:
+                            Console.Write("туз ");
+                            break;
+                    }
+                    switch (j)
+                    {
+                        case 1:
+                            Console.WriteLine("пика");
+                            break;
+                        case 2:
+                            Console.WriteLine("трефа");
+                            break;
+                        case 3:
+                            Console.WriteLine("бубна");
+                            break;
+                        case 4:
+                            Console.WriteLine("черва");
+                            break;
+                    }
+                }
             }
-
         }
     }
 }
